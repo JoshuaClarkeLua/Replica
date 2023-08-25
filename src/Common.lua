681ee419bdba5346d32e39e642a0adb7741d8a7e
JCL <joshuaclarke.lua@gmail.com>
2023-08-25 17:45:06 -0400

    - Fixed NewKey recursion. Now NewKey listeners are called from child to parent key.

--- src/Common.lua
+++ src/Common.lua
@@ -192,19 +192,25 @@ function Common.connectReplicaSignal(self: any, signalName: string, path: Path?,
 	return signal:Connect(listener)
 end
 
+
+local function _newKeyRecursive(self: any, pathTable, _tempPathTable, _pointer, i): ()
+	if i == 0 or i > #pathTable then return end
+	local newKey = pathTable[i]
+	local newValue = _pointer[newKey]
+	table.insert(_tempPathTable, newKey)
+	_newKeyRecursive(self, pathTable, _tempPathTable, newValue, i + 1)
+	table.remove(_tempPathTable, i)
+	fireReplicaSignal(self, "_OnNewKey", _tempPathTable, newKey, newValue)
+end
+
 function Common._onSetValue(self: any, pathTable: PathTable, newKeyIndex: number?, pointer:{[PathIndex]: any}, index: PathIndex, value: any): ()
 	local old = pointer[index]
 	pointer[index] = value
 	if old == nil then
-		if newKeyIndex ~= nil and newKeyIndex > 1 then
-			local _tempPathTable = table.move(pathTable, 1, newKeyIndex - 1, 1, {})
-			for i = newKeyIndex, #pathTable do
-				local newKey = pathTable[i]
-				fireReplicaSignal(self, "_OnNewKey", _tempPathTable, newKey, pointer)
-				table.insert(_tempPathTable, newKey)
-			end
-		end
-		fireReplicaSignal(self, "_OnNewKey", pathTable, index, value)
+		local _newKeyIndex = newKeyIndex or #pathTable
+		local _tempPathTable = table.move(pathTable, 1, _newKeyIndex - 1, 1, {})
+		local _pointer = getTableFromPathTable(self.Data, _tempPathTable)
+		_newKeyRecursive(self, pathTable, _tempPathTable, _pointer, _newKeyIndex)
 	end
 
 	fireReplicaSignal(self, "_OnChange", pathTable, value, old)
