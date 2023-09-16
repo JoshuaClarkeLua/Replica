3272fa0764c94a5a9a49119b485ce10742c6dc04
TheBexiSS <joshuaclerke1234@gmail.com>
2023-09-16 13:53:47 -0400

    Fixed Replica.Replica type

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@ local Common = require(script.Common)
 local Controller = require(script.ReplicaController)
 local Service = require(script.ReplicaService)
 
-export type Replica = Controller.Replica & Service.Replica
+export type Replica = Common.Replica
 export type Path = Common.Path
 export type FilterName = Common.FilterName
 export type Inclusion = Service.Inclusion
