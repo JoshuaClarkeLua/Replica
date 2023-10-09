e2c751252487ae75fdf2d22c0ab1c5f89cb365f4
JCL <joshuaclarke.lua@gmail.com>
2023-10-09 18:22:32 -0400

    Added Replica:GetReplicaById() which can be used to get replica by id on client and server

--- src/init.lua
+++ src/init.lua
@@ -1,3 +1,4 @@
+local RunService = game:GetService("RunService")
 local Common = require(script.Common)
 local Controller = require(script.ReplicaController)
 local Service = require(script.ReplicaService)
@@ -7,9 +8,17 @@ export type Path = Common.Path
 export type FilterName = Common.FilterName
 export type Inclusion = Common.Inclusion
 
-return {
-	Controller = Controller,
-	Service = Service,
-	--
-	NIL = Common.NIL,
-}
+local Replica = {}
+
+function Replica:GetReplicaById(id: string): Replica?
+	if RunService:IsServer() then
+		return Service:GetReplicaById(id)
+	end
+	return Controller:GetReplicaById(id)
+end
+
+
+Replica.Controller = Controller
+Replica.Service = Service
+Replica.NIL = Common.NIL
+return Replica
